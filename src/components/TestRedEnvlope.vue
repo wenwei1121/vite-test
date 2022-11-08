eb9838dc3088974939a7a9c47d14911922df8a00
ELAND\wenweiyang <jackie19981121>
2022-11-08 18:43:43 +0800

    adjust money prizes & show currentDrawer & reset prizes, drawerNum, currentDrawer in clickdown everySort

--- src/components/TestRedEnvlope.vue
+++ src/components/TestRedEnvlope.vue
@@ -1,35 +1,41 @@
 <script setup>
-import { ref } from "vue"
+import { ref, computed } from "vue"
 import { useResultSwal } from "@/composables/useAlert"
+import { useCloned } from "@vueuse/core"
+
+const familyArr = ["tako", "takoWife", "aunt", "auntHusband", "cat", "show", "huei", "hueiWife", "sister", "sisterBoyfriend", "brother", "sun", "ning"]
 
 const sortedArr = ref([])
 const randomSortFamilyMember = () => {
-  const familyArr = ["tako", "takoWife", "aunt", "auntHusband", "cat", "show", "huei", "hueiWife", "sister", "sisterBoyfriend", "brother", "sunBoyfriend", "sun", "ning"]
   sortedArr.value = []
+  const clonedFamilyArr = [...familyArr]
   do {
-    const randomNum = getRandomNum(familyArr)
-    sortedArr.value.push(familyArr[randomNum])
-    familyArr.splice(randomNum, 1)
-  } while (familyArr.length !== 0)
+    const randomNum = getRandomNum(clonedFamilyArr)
+    sortedArr.value.push(clonedFamilyArr[randomNum])
+    clonedFamilyArr.splice(randomNum, 1)
+  } while (clonedFamilyArr.length !== 0)
+  prizes.value = useCloned(prizesSample).cloned.value
+  drawerNum.value = 0
 }
 
 const showPrize = ref({})
-const prizes = ref([
+const prizesSample = [
   { id: 0, label: "A", money: 1200 },
-  { id: 1, label: "B", money: 600 },
-  { id: 2, label: "B", money: 600 },
-  { id: 3, label: "B", money: 600 },
-  { id: 4, label: "C", money: 200 },
-  { id: 5, label: "C", money: 200 },
-  { id: 6, label: "C", money: 200 },
-  { id: 7, label: "C", money: 200 },
-  { id: 8, label: "C", money: 200 },
-  { id: 9, label: "C", money: 200 },
-  { id: 10, label: "C", money: 200 },
-  { id: 11, label: "C", money: 200 },
-  { id: 12, label: "C", money: 200 },
-  { id: 13, label: "C", money: 200 },
-])
+  { id: 1, label: "B", money: 800 },
+  { id: 2, label: "C", money: 600 },
+  { id: 3, label: "C", money: 600 },
+  { id: 4, label: "D", money: 200 },
+  { id: 5, label: "D", money: 200 },
+  { id: 6, label: "D", money: 200 },
+  { id: 7, label: "D", money: 200 },
+  { id: 8, label: "D", money: 200 },
+  { id: 9, label: "D", money: 200 },
+  { id: 10, label: "D", money: 200 },
+  { id: 11, label: "D", money: 200 },
+  { id: 12, label: "D", money: 200 },
+]
+
+const prizes = ref(useCloned(prizesSample).cloned.value)
 const randomPrize = () => {
   if (prizes.value.length === 0) {
     useResultSwal({
@@ -48,15 +54,44 @@ const randomPrize = () => {
     timer: 0
   })
   prizes.value.splice(randomNum, 1)
+  drawerNum.value++
 }
+
+const drawerNum = ref(0)
 
 const getRandomNum = arr => {
   return Math.floor(Math.random() * arr.length)
 }
+
+const currentDrawer = computed(() => {
+  if (sortedArr.value.length) {
+    return sortedArr.value[drawerNum.value]
+  }
+
+  return ""
+})
+
+// const gameOverResult = ref([])
 </script>
 
 <template>
-  <button @click="randomSortFamilyMember">
+  <span>參加成員</span>
+  <div>
+    <ul>
+      <li
+        v-for="name of familyArr"
+        :key="name"
+      >
+        <span>
+          {{ name }}
+        </span>
+      </li>
+    </ul>
+  </div>
+  <button
+    class="bg-black text-white"
+    @click="randomSortFamilyMember"
+  >
     隨機排序
   </button>
   <div>
@@ -71,7 +106,14 @@ const getRandomNum = arr => {
       </li>
     </ul>
   </div>
-  <button @click="randomPrize">
+  <div>
+    <span>目前抽獎人: {{ currentDrawer }}</span>
+  </div>
+  <button
+    :class="[sortedArr.length ? 'bg-black text-white' : 'text-gray-300']"
+    :disabled="!sortedArr.length"
+    @click="randomPrize"
+  >
     抽獎
   </button>
   <div>
@@ -86,7 +128,7 @@ const getRandomNum = arr => {
         </span>
       </li>
     </ul>
-    <span>無獎項</span>
+    <span v-else>無獎項</span>
   </div>
 </template>
 
