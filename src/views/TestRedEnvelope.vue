0514412b33d35abebe29103b6bdd960215d5fa97
wenwei1121 <wenweiyang1121@gmail.com>
2022-11-14 00:16:47 +0800

    add flowbite table components to members and prizes

--- src/views/TestRedEnvelope.vue
+++ src/views/TestRedEnvelope.vue
@@ -28,45 +28,27 @@ const {
             scope="col"
             class="py-3 px-6"
           >
-            Product name
+            成員
           </th>
         </tr>
       </thead>
       <tbody>
-        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+        <tr
+          v-for="name of familyArr"
+          :key="name"
+          class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+        >
           <th
             scope="row"
             class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
           >
-            Apple MacBook Pro 17"
+            {{ name }}
           </th>
-          <td class="py-4 px-6">
-            Sliver
-          </td>
-          <td class="py-4 px-6">
-            Laptop
-          </td>
-          <td class="py-4 px-6">
-            $2999
-          </td>
         </tr>
       </tbody>
     </table>
   </div>
 
-  <span>參加成員</span>
-  <div>
-    <ul>
-      <li
-        v-for="name of familyArr"
-        :key="name"
-      >
-        <span>
-          {{ name }}
-        </span>
-      </li>
-    </ul>
-  </div>
   <button
     class="bg-black text-white"
     @click="randomSortFamilyMember"
@@ -96,18 +78,43 @@ const {
     抽獎
   </button>
   <div>
-    <span>剩餘獎項</span>
-    <ul v-if="prizes.length">
-      <li
-        v-for="(item, index) of prizes"
-        :key="item.id"
-      >
-        <span>
-          {{ (index + 1) + " : " + item.label + " > " + item.money }}
-        </span>
-      </li>
-    </ul>
-    <span v-else>無獎項</span>
+    <div class="overflow-x-auto relative">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th
+              scope="col"
+              class="py-3 px-6"
+            >
+              級別
+            </th>
+            <th
+              scope="col"
+              class="py-3 px-6"
+            >
+              獎金
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="item of prizes"
+            :key="item.id"
+            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+          >
+            <th
+              scope="row"
+              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              {{ item.label }}賞
+            </th>
+            <td class="py-4 px-6">
+              {{ item.money }}元
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
   <div>
     <span>抽獎結果</span>
