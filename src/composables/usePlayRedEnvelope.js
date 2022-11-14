2ec17f5908fb6f63237a47246a8a26bcb8ee76d3
ELAND\wenweiyang <jackie19981121>
2022-11-14 18:17:50 +0800

    play red envelope use table component UI & logic and store sortedAtt change use MAP

--- src/composables/usePlayRedEnvelope.js
+++ src/composables/usePlayRedEnvelope.js
@@ -1,26 +1,11 @@
-import { ref, computed } from "vue"
+import { ref, reactive, computed } from "vue"
 // composables
-import { useResultSwal } from "@/composables/useAlert"
+import { useConfirmSwal, useResultSwal } from "@/composables/useAlert"
 // vueuse
 import { useCloned } from "@vueuse/core"
 
 export const usePlay = () => {
     const familyArr = ["tako", "takoWife", "aunt", "auntHusband", "cat", "show", "huei", "hueiWife", "sister", "sisterBoyfriend", "brother", "sun", "ning"]
-
-    const sortedArr = ref([])
-    const randomSortFamilyMember = () => {
-        sortedArr.value = []
-        const clonedFamilyArr = [...familyArr]
-        do {
-            const randomNum = getRandomNum(clonedFamilyArr)
-            sortedArr.value.push(clonedFamilyArr[randomNum])
-            clonedFamilyArr.splice(randomNum, 1)
-        } while (clonedFamilyArr.length !== 0)
-        prizes.value = useCloned(prizesSample).cloned.value
-        gameOverResult.value = []
-        drawerNum.value = 0
-    }
-
     const prizesSample = [
         { id: 0, label: "A", money: 1200 },
         { id: 1, label: "B", money: 800 },
@@ -38,45 +23,48 @@ export const usePlay = () => {
     ]
 
     const prizes = ref(useCloned(prizesSample).cloned.value)
-    const showPrize = ref({})
-    const gameOverResult = ref([])
-    const drawerNum = ref(0)
-    const currentDrawer = computed(() => sortedArr.value.length ? sortedArr.value[drawerNum.value] : "")
+    const sortedArr = reactive(new Map())
+    const sortedArrKeys = computed(() => sortedArr.keys())
+    const currentDrawer = ref("")
+
+    const randomSortFamilyMember = async () => {
+        let checkResult = true
+        if (sortedArr.size && prizes.value.length) {
+            checkResult = await useConfirmSwal({ title: "尚有獎項, 確定要重排序?" })
+        }
+        if (!checkResult) return
+
+        sortedArr.clear()
+        const clonedFamilyArr = [...familyArr]
+        do {
+            const randomNum = getRandomNum(clonedFamilyArr)
+            sortedArr.set(clonedFamilyArr[randomNum], "尚未抽獎")
+            clonedFamilyArr.splice(randomNum, 1)
+        } while (clonedFamilyArr.length !== 0)
+        prizes.value = useCloned(prizesSample).cloned.value
+        currentDrawer.value = sortedArrKeys.value.next().value
+    }
 
     const randomPrize = () => {
-        if (prizes.value.length === 0) {
-            useResultSwal({
-                title: "獎已空",
-                icon: "error",
-                timer: 2000
-            })
-            return
-        }
         const randomNum = getRandomNum(prizes.value)
-        showPrize.value = prizes.value[randomNum]
-        prizes.value.splice(randomNum, 1)
+        const { label, money } = prizes.value[randomNum]
+        sortedArr.set(currentDrawer.value, `${label}賞 > ${money}元`)
         useResultSwal({
-            title: `恭喜抽中${showPrize.value.label}賞 獎金: ${showPrize.value.money}`,
-            icon: "warning",
-            showConfirmButton: true,
-            timer: 0
+            title: `恭喜抽中${label}賞 獎金: ${money}`,
+            showConfirmButton: true
         })
-        gameOverResult.value.push({
-            drawer: currentDrawer.value,
-            prizeItem: showPrize.value
-        })
-        drawerNum.value++
+        prizes.value.splice(randomNum, 1)
+        currentDrawer.value = sortedArrKeys.value.next().value
     }
 
     const getRandomNum = arr => Math.floor(Math.random() * arr.length)
 
     return {
+        prizes,
         familyArr,
         sortedArr,
+        currentDrawer,     
         randomSortFamilyMember,
-        prizes,
-        gameOverResult,
-        currentDrawer,
-        randomPrize,
+        randomPrize
     }
 }
--- src/views/TestRedEnvelope.vue
+++ src/views/TestRedEnvelope.vue
@@ -8,126 +8,183 @@ const { setCurrentPath } = useCurrentPath()
 setCurrentPath("/TestRedEnvelope")
 
 const {
+  prizes,
   familyArr,
   sortedArr,
-  randomSortFamilyMember,
-  prizes,
-  gameOverResult,
   currentDrawer,
+  randomSortFamilyMember,
   randomPrize
 } = usePlay()
 
 </script>
 
 <template>
-  <div class="overflow-x-auto relative">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-        <tr>
-          <th
-            scope="col"
-            class="py-3 px-6"
-          >
-            成員
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="name of familyArr"
-          :key="name"
-          class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+  <div class="w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex flex-col items-center p-8">
+      <span>目前抽獎人</span>
+      <img
+        class="mt-5 mb-3 w-24 h-24 rounded-full shadow-lg"
+        src="/docs/images/people/profile-picture-3.jpg"
+        alt="Bonnie image"
+      />
+      <h5 class="text-xl font-medium text-gray-900 dark:text-white">
+        {{ currentDrawer }}
+      </h5>
+      <div class="flex mt-5 space-x-3 md:mt-6">
+        <button
+          :disabled="!sortedArr.size || !prizes.length"
+          type="button"
+          class="inline-flex items-center py-2 px-4 font-medium rounded-lg text-sm text-center"
+          :class="[
+            sortedArr.size || !prizes.length
+              ? 'cursor-pointer text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800'
+              : 'cursor-not-allowed text-gray-300 bg-gray-500'
+          ]"
+          @click="randomPrize"
         >
-          <th
-            scope="row"
-            class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-          >
-            {{ name }}
-          </th>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-
-  <button
-    class="bg-black text-white"
-    @click="randomSortFamilyMember"
-  >
-    隨機排序
-  </button>
-  <div>
-    <ul>
-      <li
-        v-for="(name, index) of sortedArr"
-        :key="name"
-      >
-        <span>
-          {{ (index + 1) + " : " + name }}
-        </span>
-      </li>
-    </ul>
-  </div>
-  <div>
-    <span>目前抽獎人: {{ currentDrawer }}</span>
-  </div>
-  <button
-    :class="[sortedArr.length ? 'bg-black text-white' : 'text-gray-300']"
-    :disabled="!sortedArr.length"
-    @click="randomPrize"
-  >
-    抽獎
-  </button>
-  <div>
-    <div class="overflow-x-auto relative">
-      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th
-              scope="col"
-              class="py-3 px-6"
-            >
-              級別
-            </th>
-            <th
-              scope="col"
-              class="py-3 px-6"
-            >
-              獎金
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="item of prizes"
-            :key="item.id"
-            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-          >
-            <th
-              scope="row"
-              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              {{ item.label }}賞
-            </th>
-            <td class="py-4 px-6">
-              {{ item.money }}元
-            </td>
-          </tr>
-        </tbody>
-      </table>
+          抽獎
+        </button>
+      </div>
     </div>
   </div>
-  <div>
-    <span>抽獎結果</span>
-    <ul>
-      <li
-        v-for="(item) of gameOverResult"
-        :key="item.drawer"
-      >
-        <span>
-          {{ item.drawer + " : " + item.prizeItem.label + " > " + item.prizeItem.money }}
-        </span>
-      </li>
-    </ul>
+  <div class="w-full max-w-xl bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex flex-col items-center px-4 py-8">
+      <span>抽獎結果</span>
+      <div class="mt-5 overflow-x-auto relative">
+        <table class="w-100 text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th
+                scope="col"
+                class="py-3 px-6"
+              >
+                順序
+              </th>
+              <th
+                scope="col"
+                class="py-3 px-6"
+              >
+                成員
+              </th>
+              <th
+                scope="col"
+                class="py-3 px-6"
+              >
+                抽中獎項
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(keyName, index) of sortedArr.keys()"
+              :key="keyName"
+              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+            >
+              <th
+                scope="row"
+                class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                {{ index + 1 }}
+              </th>
+              <td class="py-4 px-6">
+                {{ keyName }}
+              </td>
+              <td class="py-4 px-6">
+                {{ sortedArr.get(keyName) }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="flex mt-5 space-x-3 md:mt-6">
+        <button
+          type="button"
+          class="
+          text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl
+            focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800
+            cursor-pointer inline-flex items-center font-medium rounded-lg text-sm py-2 px-4 text-center mr-2 mb-2
+          "     
+          @click="randomSortFamilyMember"
+        >
+          隨機排序
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="w-full max-w-xl bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex justify-evenly py-8">
+      <div class="flex flex-col items-center">
+        <span>剩餘獎項</span>
+        <div class="mt-5 overflow-x-auto relative">
+          <table class="text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th
+                  scope="col"
+                  class="py-3 px-6"
+                >
+                  成員
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="name of familyArr"
+                :key="name"
+                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+              >
+                <th
+                  scope="row"
+                  class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {{ name }}
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="flex flex-col items-center">
+        <span>剩餘獎項</span>
+        <div class="mt-5 overflow-x-auto relative">
+          <table class="w-50 text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th
+                  scope="col"
+                  class="py-3 px-6"
+                >
+                  級別
+                </th>
+                <th
+                  scope="col"
+                  class="py-3 px-6"
+                >
+                  獎金
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="item of prizes"
+                :key="item.id"
+                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+              >
+                <th
+                  scope="row"
+                  class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {{ item.label }}賞
+                </th>
+                <td class="py-4 px-6">
+                  {{ item.money }}元
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
